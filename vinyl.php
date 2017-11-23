54cccc9aa83fbc38e6c5844924489e1570702289
Janjos <joaoanjos_96@hotmail.com>
2017-11-23 15:51:47 +0000

    Modals + Header e Vinyl Page

---
+++ vinyl.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Vinyl Records, lda</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+</head>
+<body>
+
+<?php
+session_start();
+?>
+
+<?php
+include 'parts/navbar.php';
+?>
+
+<div>
+    <br>
+    <br>
+    <br>
+</div>
+
+
+<div class="container">
+    <h2>Vinyl 10</h2>
+
+
+
+</div>
+
+
+</body>
+</html>
