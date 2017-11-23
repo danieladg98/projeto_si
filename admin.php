f6679bae698584430ad1a8c9a134b8fb031b304d
danieladg98 <33914495+danieladg98@users.noreply.github.com>
2017-11-23 18:03:00 +0000

    tracks

--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@
         <input type="text" name="artist" value=""/>
         <br/>
         <br/>
-        <label for="price">Release Date</label>
+        <label for="price">Release Date (yyyy-mm-dd)</label>
         <br/>
         <input type="text" name="release_date" value=""/>
         <br/>
@@ -61,7 +61,7 @@
         <input type="text" name="description" value=""/>
         <br/>
         <br/>
-        <label for="stock">Tracks</label>
+        <label for="stock">Tracks (separed by commas)</label>
         <br/>
         <input type="text" name="tracks" value=""/>
 
@@ -82,9 +82,9 @@ if (!$conn) {
 }
 
 //Verifica se todos os campos do formulário foram preenchidos e não estão vazios
-if ((getimagesize($FILES['image']['tmp-name'])==TRUE) && (isset($_POST['name']) && !empty($_POST['name']) && (isset($_POST['artist']) && !empty($_POST['artist']) && (isset($_POST['release_date']) && !empty($_POST['release_date']) && (isset($_POST['genre']) && !empty($_POST['genre']) && (isset($_POST['price']) && !empty($_POST['price']) && (isset($_POST['stock']) && !empty($_POST['stock'])) && (isset($_POST['description']) && !empty($_POST['description']) && (isset($_POST['tracks']) && !empty($_POST['tracks']))){
-    $image= adslashes($FILES['image']['tmp-name']);
-    $imagem= image_get_contents($image);
+if ((getimagesize($_FILES['image']['tmp_name'])==TRUE) && (isset($_POST['name']) && !empty($_POST['name'])) && (isset($_POST['artist']) && !empty($_POST['artist'])) && (isset($_POST['release_date']) && !empty($_POST['release_date'])) && (isset($_POST['genre']) && !empty($_POST['genre'])) && (isset($_POST['price']) && !empty($_POST['price'])) && (isset($_POST['stock']) && !empty($_POST['stock'])) && (isset($_POST['description']) && !empty($_POST['description'])) && (isset($_POST['tracks']) && !empty($_POST['tracks']))){
+    $image= addslashes($_FILES['image']['tmp_name']);
+    $imagem= file_get_contents($image);
     $imagem= base64_encode($imagem);
 
 $escapedAlbumName = mysqli_real_escape_string($conn, $_POST['name']);
@@ -95,29 +95,37 @@ $escapedPrice = mysqli_real_escape_string($conn, $_POST['price']);
 $escapedStock = mysqli_real_escape_string($conn, $_POST['stock']);
 $escapedDescription = mysqli_real_escape_string($conn, $_POST['description']);
 $escapedTracks = mysqli_real_escape_string($conn, $_POST['tracks']);
+$arrayTracks = explode(',', $escapedTracks);
 
-//Verifica se o email já existe na base de dados
+
+//Verifica se o album já existe na base de dados
 $resultados = mysqli_query($conn, "select name,artist from albums where name='$escapedAlbumName' AND artist='$escapedArtist'");
 if (mysqli_num_rows($resultados) > 0) {
     $linha = mysqli_fetch_assoc($resultados);
     if ($escapedAlbumName == $linha['name'] && $escapedArtist == $linha['artist'] ) {
         print "This album already exists!";
     }
-    //caso o mail nao existir corre o codigo abaixo
+    //caso o album nao existir corre o codigo abaixo
 } else {
-    $msg = 'Album added successfully!';
-
-    //inserção dos dados na base de dados
+    //inserção dos dados na tabela albums
     mysqli_query($conn, "INSERT INTO albums (name , description , release_date , genre, artist, price, image, stock, active) VALUES(
             '" . mysqli_real_escape_string($conn, ucwords($escapedAlbumName)) . "',
-            '" . mysqli_real_escape_string($conn, $escapedDescription) . "',
+            '" . mysqli_real_escape_string($conn, ucwords($escapedDescription)) . "',
             '" . mysqli_real_escape_string($conn, $escapedReleaseDate) . "',
-            '" . mysqli_real_escape_string($conn, $escapedGenre) . "',
+            '" . mysqli_real_escape_string($conn, ucwords($escapedGenre)) . "',
            '" . mysqli_real_escape_string($conn, ucwords($escapedArtist)) . "',
             '" . mysqli_real_escape_string($conn, $escapedPrice) . "',
-            '" . $image . "',
+            '{$image}',
             '" . mysqli_real_escape_string($conn, $escapedStock) . "',
              '" . mysqli_real_escape_string($conn, '1') . "')") or die("Error: " . mysqli_connect_error());
+    //inserção dos dados na tabela musics
+    for($x = 0; $x < count($arrayTracks); $x++) {
+        mysqli_query($conn, "INSERT INTO musics (name , albums_id) VALUES(
+            '" . mysqli_real_escape_string($conn, $arrayTracks[$x]) . "',
+             '" . mysqli_real_escape_string($conn, $linha['id']) . "')") or die("Error: " . mysqli_connect_error());
+    }
+
+    print "Album added successfully!";
 }
 } else {
     print "Please fill all the fields!";
