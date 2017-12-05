07b9166bc3c42d9713592eee7394201436765bef
Janjos <joaoanjos_96@hotmail.com>
2017-12-05 23:37:19 +0000

    metade do search

---
+++ searchRedirect.php
@@ -0,0 +1,64 @@
+<?php
+
+//Ligação à base de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$bd = "Projeto_Si";
+$conn = mysqli_connect($servername, $username, $password, $bd);
+if (!$conn) {
+    die("Erro na ligacao: " . mysqli_connect_error()); //Mensagem de erro caso nao haja ligação à base de dados
+    //Caso haja ligação executa o código abaixo!vv
+}
+
+//Check whether userInput exists or not
+
+if(isset($_POST['search_submit'])){
+
+    if (isset($_POST['userInput'])) {
+        $value = $_POST['userInput']; //assign the value
+    } else {
+        $value = "";
+    }
+//Select query
+
+    $sql = "SELECT name, id FROM albums WHERE name LIKE '%" . $value . "%';";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+//Store the result in an array list[]
+
+            while ($row = mysqli_fetch_array($result)) {
+                $listname[] = $row['name'];
+                $listid[] = $row['id'];
+            }
+        } else {
+//set a null value to list[] if no result to prevent error
+            $listname[] = "";
+            $listid[] = "";
+        }
+    }
+
+    if (!empty($value)) {
+        if ($matched = preg_grep('~' . $value . '~', $listname)) {
+            $count = 0;
+            echo '<ul>';
+            while ($count < 5) {
+                if (isset($matched[$count])) {
+                    /*
+                    link the real target search file which should be opened when we click the search button or select the one from the drop down suggestion.
+                    */
+                    //echo '<a href=vinyl.php?id=' . $listid[$count] . '><li>' . $matched[$count] . '</li></a>';
+                    print implode(",", $listid);
+                }
+                $count++;
+            }
+            echo '</ul>';
+        } else {
+            echo "No result";
+        }
+    }
+
+}
+
+
+?>
---
+++ searchResult.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Vinyl Records, lda</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+</head>
+<body>
+
+<?php
+session_start();
+?>
+
+<?php
+include_once 'parts/navbar.php';
+?>
+
+<div>
+    <br>
+    <br>
+    <br>
+</div>
+
+
+<div class="container">
+
+    <h2>Search</h2>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $bd = "Projeto_Si";
+    $conn = mysqli_connect($servername, $username, $password, $bd);
+    if (!$conn) {
+        die("Erro na ligacao: " . mysqli_connect_error()); //Mensagem de erro caso nao haja ligação à base de dados
+        //Caso haja ligação executa o código abaixo!vv
+    }
+
+    $resultados = mysqli_query($conn, "select id, artist, name, genre, price from albums where active = 1;");
+    $nrows = ceil(mysqli_num_rows($resultados)/3);
+
+    for ($i = 0; $i < $nrows; $i++) {
+        print "<div class='row'>";
+        for($j = 0; $j < 3; $j++) {
+            while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                print "<a class='mx-auto' href='vinyl.php?id=".$linha['id']."'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='img/cerdo%20patatero.jpg' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>".$linha['name']."</h4>
+                            <p class='card-text'>".$linha['artist']."</p>
+                            <p class='card-text'>€ ".$linha['price']."</p>
+                        </div>
+                    </div></a>
+                  ";
+
+            }
+        }
+        print "</div>";
+    }
+
+    print($_GET("list"));
+    ?>
+
+
+</div>
+
+
+</body>
