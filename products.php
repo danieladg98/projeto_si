7bdb0e7fab6251aa45e46d40fdd81b1c2de04753
danieladg98 <33914495+danieladg98@users.noreply.github.com>
2017-12-15 15:34:54 +0000

    order by

--- products.php
+++ products.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="products.css" rel="stylesheet">
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -37,11 +38,36 @@ include_once 'parts/navbar.php';
 
 
     <div class="row">
-                <div class="col-3">
+        <div class="col-1">
 
+        </div>
+                <div class="lateral col-2">
+                    <h2>OUR</h2>
+                    <h2>PRODUCTS</h2>
+                    <br>
+                <form method="post" action="">
+                    <input type="submit" name="newreleases" value="NEW RELEASES">
+                    <input type="submit" name="mostwanted" value="MOST WANTED"><br>
+                    <input type="submit" name="all" value="GENRE">
+                    <div class="col-1">
+                        <div class="in">
+                        <input type="submit" name="all" value="All">
+                        <input type="submit" name="blues" value="Blues">
+                        <input type="submit" name="classical" value="Classical">
+                        <input type="submit" name="country" value="Country">
+                        <input type="submit" name="electronic" value="Electronic">
+                        <input type="submit" name="indie" value="Indie">
+                        <input type="submit" name="latin" value="Latin">
+                        <input type="submit" name="rap" value="Rap">
+                        <input type="submit" name="raggae" value="Raggae">
+                        <input type="submit" name="rock" value="Rock">
+                        <input type="submit" name="rb" value="R&B">
+                        </div>
+                    </div>
+                </form>
                 </div>
+
         <div class="col-9">
-            <h2>Our Products</h2>
 
             <?php
 
@@ -54,28 +80,270 @@ include_once 'parts/navbar.php';
                 die("Erro na ligacao: " . mysqli_connect_error()); //Mensagem de erro caso nao haja ligação à base de dados
                 //Caso haja ligação executa o código abaixo!vv
             }
+            if (isset($_POST['newreleases'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 order by release_date desc limit 6");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
 
-            $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 order by release_date desc");
-            $nrows = ceil(mysqli_num_rows($resultados)/3);
-
-            for ($i = 0; $i < $nrows; $i++) {
-                print "<div class='row'>";
-                for($j = 0; $j < 3; $j++) {
-                    while ($linha = mysqli_fetch_assoc($resultados)) {
-
-                        print "<a class='mx-left' href='vinyl.php?id=".$linha['id']."'><div class='card border-0' style='width: 20rem;'>
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
                         <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
                         <div class='card-block'>
-                            <h4 class='card-title'>".$linha['name']."</h4>
-                            <p class='card-text'>".$linha['artist']."</p>
-                            <p class='card-text'>€ ".$linha['price']."</p>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
                         </div>
                     </div></a>
                   ";
 
+                        }
                     }
+                    print "</div>";
                 }
-                print "</div>";
+            } else if (isset($_POST['all'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['blues'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Blues' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['classical'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Classical' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['country'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Country' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['electronic'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Electronic' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['indie'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Indie' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['latin'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Latin' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['rap'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'rap' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['raggae'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Raggae' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['rock'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'Rock' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
+            } else if (isset($_POST['rb'])) {
+                $resultados = mysqli_query($conn, "select id, artist, name, genre, release_date, price, image from albums where active = 1 AND genre = 'R&B' order by release_date desc");
+                $nrows = ceil(mysqli_num_rows($resultados) / 3);
+                for ($i = 0; $i < $nrows; $i++) {
+                    print "<div class='row'>";
+                    for ($j = 0; $j < 3; $j++) {
+                        while ($linha = mysqli_fetch_assoc($resultados)) {
+
+                            print "<a class='mx-left' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
+                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
+                        <div class='card-block'>
+                            <h4 class='card-title'>" . $linha['name'] . "</h4>
+                            <p class='card-text'>" . $linha['artist'] . "</p>
+                            <p class='card-text'>€ " . $linha['price'] . "</p>
+                        </div>
+                    </div></a>
+                  ";
+
+                        }
+                    }
+                    print "</div>";
+                }
             }
 
 
@@ -87,3 +355,4 @@ include_once 'parts/navbar.php';
 
 
 </body>
+</html>
