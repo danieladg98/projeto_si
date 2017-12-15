b1ecbfc0fabf1ae889d7951a20a516d7a2070eab
Janjos <joaoanjos_96@hotmail.com>
2017-12-15 23:18:17 +0000

    search complete

--- searchResult.php
+++ searchResult.php
@@ -57,7 +57,7 @@ include_once 'parts/navbar.php';
             $searchInput = $_POST['searchInput'];
 
             $searchAlbum = mysqli_query($conn, "SELECT * FROM albums WHERE name LIKE '%" . $searchInput . "%';");
-            $searchMusic = mysqli_query($conn, "SELECT * FROM musics, albums WHERE musics.albums_id=albums.id and musics.name LIKE '%" . $searchInput . "%';");
+            $searchMusic = mysqli_query($conn, "SELECT musics.name as musics_name, albums_id, id, albums.name, artist, active FROM musics, albums WHERE musics.albums_id=albums.id and musics.name LIKE '%" . $searchInput . "%';");
             $rowsAlbum = mysqli_num_rows($searchAlbum);
             $rowsMusic = mysqli_num_rows($searchMusic);
 
@@ -85,35 +85,38 @@ include_once 'parts/navbar.php';
                     }
                     print "</div>";
                 }
+            } else {
+                print "<h5>Searching for albuns:</h5>";
+                print "Nothing to show";
             }
             if ($rowsMusic > 0) {
-                {
+
                     print "<h5>Searching for Musics:</h5>";
-                    for ($i = 0; $i < $nrowsM; $i++) {
-                        print "<div class='row'>";
-                        for ($j = 0; $j < 3; $j++) {
-                            while ($linha = mysqli_fetch_assoc($searchMusic)) {
-
-                                print "<a class='mx-3' href='vinyl.php?id=" . $linha['id'] . "'><div class='card border-0' style='width: 20rem;'>
-                        <img class='card-img-top' src='" . $linha['image'] . "' alt='Card image cap'>
-                        <div class='card-block'>
-                            <h4 class='card-title'>" . $linha['name'] . "</h4>
-                            <p class='card-text'>" . $linha['artist'] . "</p>
-                            <p class='card-text'>€ " . $linha['price'] . "</p>
-                        </div>
-                    </div></a>
-                  ";
-
+                    for ($i = 0; $i < $rowsMusic; $i++) {
+                        while ($linha = mysqli_fetch_assoc($searchMusic)) {
+                            $musicas = explode(",",$linha['musics_name']);
+                            if ($matched = preg_grep('~' . $searchInput . '~i', $musicas)) {
+                                $count = 0;
+                                print '<ul>';
+                                while ($count < count($musicas)) {
+                                    if (isset($matched[$count])) {
+                                        /*
+                                        link the real target search file which should be opened when we click the search button or select the one from the drop down suggestion.
+                                        */
+                                        echo "<a href=vinyl.php?id='" . $linha['albums_id'] . "'><li>" . $linha['artist'] . " - " . $matched[$count] . "</li></a>";
+                                    }
+                                    $count++;
+                                }
+                                print '</ul>';
                             }
                         }
-                        print "</div>";
                     }
                 }
 
             } else {
                 print "No data";
             }
-        }}
+        }
 
 
     ?>
